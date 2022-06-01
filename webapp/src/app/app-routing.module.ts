1b826c7f662d76d509991087b0194d3ffbb71113
Dany-st <aleinad971@gmail.com>
2022-05-31 20:42:42 -0500

    components

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FruitsComponent } from './fruits/fruits.component';
+import { VegetablesComponent } from './vegetables/vegetables.component';
+import { DairyComponent } from './dairy/dairy.component';
+import { MycartComponent } from './mycart/mycart.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import { ContactComponent } from './contact/contact.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path: 'home', component: HomeComponent},
+{path: 'fruits', component: FruitsComponent},
+{path: 'vegetables', component: VegetablesComponent},
+{path: 'dairy', component: DairyComponent},
+{path: 'mycart', component: MycartComponent},
+{path: 'about', component: AboutComponent},
+{path: 'search', component: SearchComponent},
+{path: 'contact', component: ContactComponent},
+{path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
