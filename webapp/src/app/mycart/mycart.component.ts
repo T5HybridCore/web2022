8452cf041b9454f696d1d713a71fa09bef704680
Uriel <urivanre3@gmail.com>
2022-06-14 16:02:04 -0500

    commit cart

--- webapp/src/app/mycart/mycart.component.ts
+++ webapp/src/app/mycart/mycart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../shared/services/api.service';
 
 @Component({
   selector: 'app-mycart',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MycartComponent implements OnInit {
 
-  constructor() { }
+    //carrito a imprimir
+    cart: any = {};
+    //producto a imprimir 
+    product: any = {};
+
+    //variables de conteo
+    counter:number=0;
+    accumulator:number=0;
+
+  //bandera marca cuando esta disponible el carrito
+  loadingCart: boolean;
 
   ngOnInit(): void {
   }
+
+  constructor(private router: ActivatedRoute, private api: ApiService) {
+
+    //buscar el carrito del usuario por id, regresa id de productos
+    this.loadingCart = true;
+    console.log("dentro de CART CONSTRU");
+    
+    this.router.params.subscribe(params => {
+      this.getProduct(params['id']);
+    
+      //prueba en consola
+      console.log("llego " + params['id']);
+    });
+  }
+
+  getProduct(id: string) {
+
+    console.log("dentro get CART");
+    this.loadingCart = true;
+
+    //se obtienen los datos del carrito seleccionado
+    this.api.getProduct(id).subscribe(product => {
+      console.log("ver producto PRODUCT = "+ product);
+      console.log(product);
+      
+      //se meten los datos de la BD a nuestra variable
+      this.product = product; 
+
+      //prueba en consola
+      console.log(this.product.Title );
+      console.log(this.product.Manufacturer );
+      console.log(this.product.Price );
+      this.loadingCart = false;
+
+    });
+  }
+
+
+
 
 }
--- webapp/src/app/ver-un-producto/ver-un-producto.component.ts
+++ webapp/src/app/ver-un-producto/ver-un-producto.component.ts
@@ -12,16 +12,25 @@ import { ApiService } from '../shared/services/api.service';
 })
 export class VerUnProductoComponent implements OnInit {
 
+  //producto a imprimir
   product: any = {};
+
+  //cantidad a comprar  
+  amount:number=0;
+
+  
+
   loadingProduct: boolean;
   
   constructor(private router: ActivatedRoute, private api: ApiService) {
 
     this.loadingProduct = true;
     console.log("dentro de ver producto CONSTRUCTOR ");
+    
     this.router.params.subscribe(params => {
       this.getProduct(params['id']);
     
+      //prueba en consola
       console.log("llego " + params['id']);
     });
   }
@@ -29,13 +38,19 @@ export class VerUnProductoComponent implements OnInit {
   ngOnInit(): void { }
 
   getProduct(id: string) {
+
     console.log("dentro de ver producto GETPRODUCT");
     this.loadingProduct = true;
+
+    //se obtienen los datos del producto seleccionado
     this.api.getProduct(id).subscribe(product => {
       console.log("ver producto PRODUCT = "+ product);
       console.log(product);
       
+      //se meten los datos de la BD a nuestra variable
       this.product = product; 
+
+      //prueba en consola
       console.log(this.product.Title );
       console.log(this.product.Manufacturer );
       console.log(this.product.Price );
