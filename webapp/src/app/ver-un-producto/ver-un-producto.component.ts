8a07f38100eb92c48056784ebd7c33ce687dc1ad
Uriel <urivanre3@gmail.com>
2022-06-16 11:57:02 -0500

    commit u

--- webapp/src/app/ver-un-producto/ver-un-producto.component.ts
+++ webapp/src/app/ver-un-producto/ver-un-producto.component.ts
@@ -8,56 +8,66 @@ import { ApiService } from '../shared/services/api.service';
 @Component({
   selector: 'app-ver-un-producto',
   templateUrl: './ver-un-producto.component.html',
-  styleUrls: ['./ver-un-producto.component.css']
+  styleUrls: ['./ver-un-producto.component.css'],
 })
 export class VerUnProductoComponent implements OnInit {
-
   //producto a imprimir
   product: any = {};
-
-  //cantidad a comprar  
-  amount:number=0;
-
+  //cantidad a comprar
   
-
   loadingProduct: boolean;
-  
-  constructor(private router: ActivatedRoute, private api: ApiService) {
 
+  constructor(private router: ActivatedRoute, private api: ApiService) {
     this.loadingProduct = true;
-    console.log("dentro de ver producto CONSTRUCTOR ");
-    
-    this.router.params.subscribe(params => {
+    console.log('dentro de ver producto CONSTRUCTOR ');
+
+    this.router.params.subscribe((params) => {
       this.getProduct(params['id']);
-    
       //prueba en consola
-      console.log("llego " + params['id']);
+      console.log('llego ' + params['id']);
     });
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
+  //metodo que busca productos
   getProduct(id: string) {
-
-    console.log("dentro de ver producto GETPRODUCT");
+    console.log('dentro de ver producto GETPRODUCT');
     this.loadingProduct = true;
 
     //se obtienen los datos del producto seleccionado
-    this.api.getProduct(id).subscribe(product => {
-      console.log("ver producto PRODUCT = "+ product);
+    this.api.getProduct(id).subscribe((product) => {
+      console.log('ver producto PRODUCT = ' + product);
       console.log(product);
-      
+
       //se meten los datos de la BD a nuestra variable
-      this.product = product; 
+      this.product = product;
 
       //prueba en consola
-      console.log(this.product.Title );
-      console.log(this.product.Manufacturer );
-      console.log(this.product.Price );
+      console.log(this.product.Title);
+      console.log(this.product.Manufacturer);
+      console.log(this.product.Price);
       this.loadingProduct = false;
-
     });
   }
 
-  
+
+  //añadir productos al carrito
+  addProduct_tocart_byid(amounts:string ) {
+    this.api.addProduct(this.product.id, amounts).subscribe((product) => {
+      console.log('ver producto PRODUCT = ' + product);
+      console.log(product);
+
+      //se meten los datos de la BD a nuestra variable
+      this.product = product;
+
+      //prueba en consola
+      console.log(this.product.Title);
+      console.log(this.product.Manufacturer);
+      console.log(this.product.Price);
+      this.loadingProduct = false;
+    });
+
+    
+  }
 }
