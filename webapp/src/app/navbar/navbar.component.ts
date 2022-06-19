4928a6fe7383f24477de842d6b84e5138fbc3150
Alex Zamarripa <T5HybridCore@gmail.com>
2022-06-19 00:01:54 -0500

    Latest

--- webapp/src/app/navbar/navbar.component.ts
+++ webapp/src/app/navbar/navbar.component.ts
@@ -1,27 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
+import { Component, OnDestroy, OnInit, Optional, ViewChild } from '@angular/core';
+import { Auth, authState, User, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { traceUntilFirst } from '@angular/fire/performance';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { EMPTY, map, Observable, Subscription } from 'rxjs';
 declare var window: any;
-
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  // View
+  @ViewChild('wrongData')
+  public readonly wrongData!: SwalComponent;
 
+  // Modal
   modal: any;
-  constructor() { }
+
+  // Attributes
+  private readonly userDisposable: Subscription | undefined;
+  public readonly user: Observable<User | null> = EMPTY;
+
+  // Form
+  form: FormGroup;
+
+  // Constructor
+  constructor(@Optional() private auth: Auth, private router: Router) {
+    if (auth) {
+      this.user = authState(this.auth);
+
+      this.userDisposable = authState(this.auth).pipe(
+        traceUntilFirst('auth'),
+        map(u => !!u)
+      ).subscribe(isLoggedIn => {
+        if (isLoggedIn) router.navigate(['/admin']);
+      });
+    }
+    
+    // Form
+    this.form = new FormGroup({
+      'email': new FormControl('', Validators.required),
+      'password': new FormControl('', Validators.required)
+    });
+  }
+
+  // OnInit
   ngOnInit(): void {
     this.modal = new window.bootstrap.Modal(
-      document.getElementById('ModalForm')
+      document.getElementById('modal')
     );
-
   }
-  openLoginForm(): void{
-    this.modal.show();
-   }
-       
-   
 
+  ngOnDestroy(): void {
+    if (this.userDisposable) {
+      this.userDisposable.unsubscribe();
+    }
+  }
+
+  openModal(): void {
+    this.modal.show();
+  }
+
+  async login() {
+    await signInWithEmailAndPassword(this.auth, this.form.value['email'], this.form.value['password'])
+      .then((user) => {
+        //this.router.navigate(['/admin']);
+        this.modal.hide();
+      })
+      .catch(async (error) => {
+        await this.wrongData.fire();        
+      });
+  }
 }
