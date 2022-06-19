5273682f408353061b0d0feb687053c496b5d250
Alex Zamarripa <T5HybridCore@gmail.com>
2022-06-19 15:17:39 -0500

    latest

--- webapp/src/app/faq/faq.component.ts
+++ webapp/src/app/faq/faq.component.ts
@@ -10,44 +10,45 @@ export class FaqComponent implements OnInit {
   mensaje: any;
   oracion: any;
 
-  constructor() {  if ('speechSynthesis' in window) {
-    this.mensaje = new SpeechSynthesisUtterance();
-  } else {
-    alert("Lo siento, tu navegador no soporta esta tecnología");
+  constructor() {
+    if ('speechSynthesis' in window) {
+      this.mensaje = new SpeechSynthesisUtterance();
+    } else {
+      alert("Lo siento, tu navegador no soporta esta tecnología");
+    }
+    // this.validarSpeak = global;
+    // console.log(this.validarSpeak);
   }
-  // this.validarSpeak = global;
-  // console.log(this.validarSpeak);
-}
 
 
-ngOnInit(): void {
-}
-
-playSpeak(a1:string) {  
-  this.oracion = document.getElementById(a1)!.innerHTML;
-  this.mensaje.text= this.oracion; 
-  if(speechSynthesis.paused){
-    speechSynthesis.resume();
-  }else{
-    speechSynthesis.cancel();
-    speechSynthesis.speak(this.mensaje);
+  ngOnInit(): void {
   }
-}
 
-stopSpeak(){
-  speechSynthesis.pause();
-}
-
-playSpeak2(a1:string,a2:string,a3:string) {
-
-  this.oracion = document.getElementById(a1)!.innerHTML + "."+document.getElementById(a2)!.innerHTML +". " + document.getElementById(a3)!.innerHTML;
-  this.mensaje.text= this.oracion; 
-  if(speechSynthesis.paused){
-    speechSynthesis.resume();
-  }else{
-    speechSynthesis.cancel();
-    speechSynthesis.speak(this.mensaje);
+  playSpeak(a1: string) {
+    this.oracion = document.getElementById(a1)!.innerHTML;
+    this.mensaje.text = this.oracion;
+    if (speechSynthesis.paused) {
+      speechSynthesis.resume();
+    } else {
+      speechSynthesis.cancel();
+      speechSynthesis.speak(this.mensaje);
+    }
   }
-}
+
+  stopSpeak() {
+    speechSynthesis.pause();
+  }
+
+  playSpeak2(a1: string, a2: string, a3: string) {
+
+    this.oracion = document.getElementById(a1)!.innerHTML + "." + document.getElementById(a2)!.innerHTML + ". " + document.getElementById(a3)!.innerHTML;
+    this.mensaje.text = this.oracion;
+    if (speechSynthesis.paused) {
+      speechSynthesis.resume();
+    } else {
+      speechSynthesis.cancel();
+      speechSynthesis.speak(this.mensaje);
+    }
+  }
 
 }
